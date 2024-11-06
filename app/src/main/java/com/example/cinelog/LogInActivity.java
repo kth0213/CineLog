ad7bd52643c0c7ed66cd0065749c8fb8314bfab3
kth0213 <asrai0412@google.com>
2024-11-06 14:20:21 +0900

    커밋 테스트

--- app/src/main/java/com/example/cinelog/LogInActivity.java
+++ app/src/main/java/com/example/cinelog/LogInActivity.java
@@ -42,8 +42,6 @@ public class LogInActivity extends AppCompatActivity {
 //            String password = passwordEditText.getText().toString();
 
             Intent intent = new Intent(this, SignUpActivity.class);
-//            intent.putExtra("EMAIL", email);
-//            intent.putExtra("PASSWORD", password);
 
             startActivity(intent);
         });
