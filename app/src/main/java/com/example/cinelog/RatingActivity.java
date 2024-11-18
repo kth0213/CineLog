36e32d09bb420ed8cbe976399ec0ad5e9f298c40
kth0213 <asrai0412@google.com>
2024-11-18 22:57:27 +0900

    feat : rating, rating list
    기록 화면, 전체 기록 화면 일부 구현,
    회원가입시 파이어스토어에 유저 등록,
    파이어스토어에 계정 별 기록 연동

--- app/src/main/java/com/example/cinelog/RatingActivity.java
+++ app/src/main/java/com/example/cinelog/RatingActivity.java
@@ -1,6 +1,11 @@
 package com.example.cinelog;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,60 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.cinelog.databinding.ActivityRatingBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class RatingActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_rating);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        ActivityRatingBinding binding = ActivityRatingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        RatingBar ratingBar = binding.ratingbar;
+        EditText commentEditText = binding.comment;
+        EditText dateEditText = binding.date;
+        EditText placeEditText = binding.place;
+        EditText friendEditText = binding.friend;
+        EditText memoEditText = binding.memo;
+        Button saveButton = binding.saveButton;
+
+
+        saveButton.setOnClickListener(view -> {
+            String comment = commentEditText.getText().toString();
+            String date = dateEditText.getText().toString();
+            String place = placeEditText.getText().toString();
+            String friend = friendEditText.getText().toString();
+            String memo = memoEditText.getText().toString();
+            Log.d("Button", "Save button clicked");
+
+
+            Map<String, Object> allData = new HashMap<>();
+            allData.put("comment", comment);
+            allData.put("date", date);
+            allData.put("place", place);
+            allData.put("friend", friend);
+            allData.put("memo", memo);
+            allData.put("rating", ratingBar.getRating());
+
+            db.collection("users/"+mAuth.getUid()+"/ratings").document(binding.title.getText().toString())
+                    .set(allData)
+                    .addOnSuccessListener(aVoid -> Log.d("Firestore", "Data saved successfully"))
+                    .addOnFailureListener(e -> Log.w("Firestore", "Error saving data", e));
+
+            Intent intent = new Intent(this, RatingListActivity.class);
+            startActivity(intent);
         });
     }
 }
