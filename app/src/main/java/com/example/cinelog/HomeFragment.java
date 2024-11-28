39a5553eb5d7db5ac26faa09dfd2088265b48b28
Yongseon Yoo <jade020823@naver.com>
2024-11-29 01:24:07 +0900

    검색 액티비티 구현

--- app/src/main/java/com/example/cinelog/HomeFragment.java
+++ app/src/main/java/com/example/cinelog/HomeFragment.java
@@ -1,12 +1,28 @@
 package com.example.cinelog;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +39,8 @@ public class HomeFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private List<String> imageUrls;
+    private RecyclerView recyclerView;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -58,7 +76,137 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ImageView searchbutton = view.findViewById(R.id.search_button);
+
+        searchbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(requireContext(), Home_SearchActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        recyclerView = view.findViewById(R.id.mylog_recyclerview);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager
+                (requireContext(),LinearLayoutManager.HORIZONTAL,false));
+
+        imageUrls = new ArrayList<>();
+
+        RecyclerView.Adapter<MovieImageViewHolder> adapter = new RecyclerView.Adapter<MovieImageViewHolder>() {
+            @NonNull
+            @Override
+            public MovieImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) { //뷰 홀더를 생성 함
+                View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.home_mylog_item,parent,false);
+
+                ViewGroup.LayoutParams layoutParams = itemView.getLayoutParams();
+                layoutParams.width = parent.getWidth() / 4; // 화면 폭의 1/4로 설정
+                itemView.setLayoutParams(layoutParams);
+
+                return new MovieImageViewHolder(itemView);
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull MovieImageViewHolder holder, int position) {
+                String imageUrl = imageUrls.get(position);
+
+
+
+
+                    Glide.with(holder.imageView.getContext())
+                            .load(imageUrl)
+                            .into(holder.imageView);
+
+
+
+            }
+
+            @Override
+            public int getItemCount() {
+                return imageUrls.size();
+            }
+        };
+
+        recyclerView.setAdapter(adapter);
+
+        loadMovieImages(adapter);
+
+
+
+    }
+
+
+    private void loadMovieImages(RecyclerView.Adapter<?> adapter){  // 리사이클러뷰에 이미지 띄우는 메서드
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("users")
+                .document(userId)
+                .collection("ratings")
+                .get()
+                .addOnSuccessListener(queryDocumentSnapshots  -> {
+                    imageUrls.clear();
+
+                    for (DocumentSnapshot document : queryDocumentSnapshots.getDocuments()) {
+                        String posterUrl = document.getString("posterPath"); // 'posterPath' 필드 가져오기
+                        if (posterUrl != null) {
+                            imageUrls.add(posterUrl); // 이미지 URL 리스트에 추가
+                        }
+                        else {
+                            System.out.println("Document missing posterPath: " + document.getId());
+                        }
+                    }
+                    adapter.notifyDataSetChanged();
+                })
+                .addOnFailureListener(e -> {
+                    e.printStackTrace();
+                });
+
+    }
+
+
+    private static class MovieImageViewHolder extends RecyclerView.ViewHolder{
+        ImageView imageView;
+
+        public MovieImageViewHolder(View itemView){
+            super(itemView);
+            imageView = itemView.findViewById(R.id.mylog_poster_image);
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
