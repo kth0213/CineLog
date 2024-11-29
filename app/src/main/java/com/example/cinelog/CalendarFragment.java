9efbfb05bdd702979e4f5858cc30a69a8b96ea59
kth0213 <asrai0412@google.com>
2024-11-29 23:09:32 +0900

    feat : calendar recyclerview 캘린더 리사이클러 뷰 1차 완성

--- app/src/main/java/com/example/cinelog/CalendarFragment.java
+++ app/src/main/java/com/example/cinelog/CalendarFragment.java
@@ -1,5 +1,6 @@
 package com.example.cinelog;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -8,40 +9,41 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CalendarFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CalendarFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
+    private FirebaseFirestore db;
+    private FirebaseAuth mAuth;
+
+    private List<Movie> movieList = new ArrayList<>();
+    private MyAdapter adapter;
+
     public CalendarFragment() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CalendarFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static CalendarFragment newInstance(String param1, String param2) {
         CalendarFragment fragment = new CalendarFragment();
         Bundle args = new Bundle();
@@ -58,6 +60,8 @@ public class CalendarFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -70,57 +74,110 @@ public class CalendarFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(),RecyclerView.VERTICAL,false));
-        //recyclerView.setAdapter(new MyAdapter());
+
+        RecyclerView recyclerView = view.findViewById(R.id.calendar_recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false));
+
+        // 어댑터 초기화
+        adapter = new MyAdapter(movieList);
+        recyclerView.setAdapter(adapter);
+
+        // 데이터 가져오기
+        fetchMovies();
+    }
+
+    private void fetchMovies() {
+        String uid = mAuth.getCurrentUser().getUid();
+
+        db.collection("users").document(uid).collection("ratings")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        // 데이터가 성공적으로 불러와졌을 때 movieList를 업데이트
+                        movieList.clear(); // 이전 데이터를 삭제
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            String title = document.getString("title");
+                            String posterPath = document.getString("posterPath");
+                            Float rating = ((Double) document.get("rating")).floatValue();
+                            String date = document.getString("date");
+
+                            if (title != null && posterPath != null) {
+                                movieList.add(new Movie(title, posterPath, rating, date));
+                            }
+                        }
+                        // 데이터 변경 후 어댑터에 알리기
+                        adapter.notifyDataSetChanged();
+                    } else {
+                        Log.e("Firestore", "Error getting documents: ", task.getException());
+                    }
+                });
     }
 
 
     public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
-        private String[] localDataSet;
+        private List<Movie> movieList;
 
+        // ViewHolder 클래스 정의
         public class ViewHolder extends RecyclerView.ViewHolder {
-            private final TextView textView;
+            private final TextView dateTextView;
+            private final TextView titleTextView;
+            private final RatingBar ratingBar;
+            private final ImageView posterImageView;
 
             public ViewHolder(View view) {
                 super(view);
+                dateTextView = view.findViewById(R.id.calendar_date);
+                titleTextView = view.findViewById(R.id.calendar_title);
+                ratingBar = view.findViewById(R.id.calendar_rating);
+                posterImageView = view.findViewById(R.id.calendar_poster);
 
-                textView = (TextView) view.findViewById(R.id.calendar_date);
-            }
 
-            public TextView getTextView() {
-                return textView;
+                view.findViewById(R.id.calendar_layout).setOnClickListener(view1 -> {
+                    int position = getBindingAdapterPosition();
+                    Movie movie = movieList.get(position);
+                    Intent intent = new Intent(view1.getContext(), RatedMovieActivity.class);
+                    intent.putExtra("title", movie.getTitle());
+                    intent.putExtra("posterPath", movie.getPosterPath());
+                    view1.getContext().startActivity(intent);
+                });
             }
         }
 
-        public MyAdapter(String[] dataSet) {
-            localDataSet = dataSet;
+        // 생성자: 데이터 리스트를 받아 저장
+        public MyAdapter(List<Movie> movieList) {
+            this.movieList = movieList;
         }
 
-        // Create new views (invoked by the layout manager)
+        // ViewHolder 생성
+        @NonNull
         @Override
-        public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-            // Create a new view, which defines the UI of the list item
-            View view = LayoutInflater.from(viewGroup.getContext())
-                    .inflate(R.layout.item_calendar, viewGroup, false);
-
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_calendar, parent, false);
             return new ViewHolder(view);
         }
 
-        // Replace the contents of a view (invoked by the layout manager)
+        // ViewHolder에 데이터 바인딩
         @Override
-        public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            Movie movie = movieList.get(position);
+            holder.dateTextView.setText(movie.getDate());
+            holder.titleTextView.setText(movie.getTitle());
+            holder.ratingBar.setRating(movie.getRating());
 
-            // Get element from your dataset at this position and replace the
-            // contents of the view with that element
-            viewHolder.getTextView().setText(localDataSet[position]);
+            Glide.with(holder.posterImageView.getContext())
+                    .load(movie.getPosterPath()) // 포스터 URL
+                    .into(holder.posterImageView);
+
+
         }
 
-        // Return the size of your dataset (invoked by the layout manager)
+        // 데이터셋 크기 반환
         @Override
         public int getItemCount() {
-            return localDataSet.length;
+            return movieList.size();
         }
     }
+
 }
