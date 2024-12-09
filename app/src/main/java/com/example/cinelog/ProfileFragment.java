c96ec5c7718713d2c34a8acebd0e0dff383336a0
Asmir0412 <Asmir0412@users.noreply.github.com>
2024-12-09 21:50:01 +0900

    feat : 프로필 프래그먼트 레이아웃 설정

--- app/src/main/java/com/example/cinelog/ProfileFragment.java
+++ app/src/main/java/com/example/cinelog/ProfileFragment.java
@@ -1,14 +1,21 @@
 package com.example.cinelog;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 public class ProfileFragment extends Fragment {
 
@@ -18,6 +25,9 @@ public class ProfileFragment extends Fragment {
 
     private String mParam1;
     private String mParam2;
+
+    private FirebaseFirestore db;
+    private FirebaseAuth mAuth;
 
     public ProfileFragment() {
     }
@@ -38,6 +48,8 @@ public class ProfileFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -50,7 +62,57 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        String uid = mAuth.getCurrentUser().getUid();
+
+        db.collection("users")
+                .document(uid)
+                .get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    if (documentSnapshot.exists()) {
+                        String nickname = documentSnapshot.getString("nickname");
+                        TextView nicknameTextView = view.findViewById(R.id.nickname);
+                        nicknameTextView.setText(nickname);
+                        Log.d("Firestore", "Nickname: " + nickname);
+                    } else {
+                        Log.d("Firestore", "Document does not exist!");
+                    }
+                })
+                .addOnFailureListener(e -> {
+                    Log.e("Firestore", "Error fetching nickname: ", e);
+                });
+
+        db.collection("users").document(uid).collection("ratings")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        int ratingCount = task.getResult().size();
+                        TextView ratingCountTextView = view.findViewById(R.id.rating_count);
+                        ratingCountTextView.setText(String.valueOf(ratingCount));
+                        Log.d("Firestore", "문서 개수: " + ratingCount);
+                    } else {
+                        Log.e("Firestore", "문서 개수 가져오기 실패: ", task.getException());
+                    }
+                });
+
+        view.findViewById(R.id.rating_count).setOnClickListener(v -> {
+            startActivity(new Intent(getActivity(), RatingListActivity.class));
+        });
+
+        view.findViewById(R.id.keyword_settingsButton).setOnClickListener(v -> {
+           startActivity(new Intent(getActivity(), KeywordSettingsActivity.class));
+        });
+
+        view.findViewById(R.id.logoutButton).setOnClickListener(v -> {
+            mAuth.signOut();
+            startActivity(new Intent(getActivity(), LogInActivity.class));
+            getActivity().finish();
+        });
+
+        view.findViewById(R.id.setting_button).setOnClickListener(v -> {
+            startActivity(new Intent(getActivity(), SettingsActivity.class));
+        });
 
 
     }
+
 }
